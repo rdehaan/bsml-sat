c0e5135f5f84d4951c676f0bff24de35e429ae7f
Ronald de Haan <me@ronalddehaan.eu>
2025-02-07 21:41:41 +0100

    Add some symmetry breaking

--- bsml_sat.py
+++ bsml_sat.py
@@ -197,7 +197,7 @@ def formula_to_asp_facts(formula):
             program += add_program
             return index, program
 
-    index, program = fact_constructor(1, formula)
+    _, program = fact_constructor(1, formula)
     return program + "formula_root(1).\n"
 
 
@@ -227,44 +227,89 @@ def solve_bsml_sat(
         { relation(W1,W2) : world(W1), world(W2) }.
 
         state_root(1).
+
         { state(N,W) : world(W) } :- state_node(N).
+
         state_nonempty(N) :- state_node(N), state(N,W).
         state_empty(N) :- state_node(N), not state(N,W) : world(W).
+
         state_type(join;relation;full_relation;leaf;inactive).
         1 { state_type(N,T) : state_type(T) } 1 :- state_node(N).
+
         :- state_type(R,inactive), state_root(R).
         :- state_type(N,inactive), state_nonempty(N).
-        1 { state_edge(N,M) : state_node(M), N != M } :- state_type(N,join), state_nonempty(N).
-        1 { state_edge(N,M) : state_node(M), N != M } :- state_type(N,relation), state_nonempty(N).
-        1 { state_edge(N,M) : state_node(M), N != M } :- state_type(N,full_relation), state_nonempty(N).
-        :- state_type(N,relation), state_edge(N,M), state_empty(M).
+
+        1 { state_edge(N,M) : state_node(M), N != M } :-
+            state_type(N,join), state_nonempty(N).
         :- state_type(N,join),
             state(N,W), not state(M,W) : state_edge(N,M).
         :- state_type(N,join),
             state_edge(N,M), state(M,W), not state(N,W).
-        1 { state_successor(M,W) : state_edge(N,M) } 1 :- state_type(N,relation), state(N,W).
+
+        1 { state_edge(N,M) : state_node(M), N != M } :-
+            state_type(N,relation), state_nonempty(N).
+        :- state_type(N,relation), state_edge(N,M), state_empty(M).
+        1 { state_successor(M,W) : state_edge(N,M) } 1 :-
+            state_type(N,relation), state(N,W).
         :- state_successor(M,W1), state(M,W2), not relation(W1,W2).
-        1 { state_full_successor(M,W) : state_edge(N,M) } 1 :- state_type(N,full_relation), state(N,W).
+
+        1 { state_edge(N,M) : state_node(M), N != M } :-
+            state_type(N,full_relation), state_nonempty(N).
+        1 { state_full_successor(M,W) : state_edge(N,M) } 1 :-
+            state_type(N,full_relation), state(N,W).
         :- state_full_successor(M,W1), state(M,W2), not relation(W1,W2).
         :- state_full_successor(M,W1), relation(W1,W2), not state(M,W2).
-        :- state_node(N), not state_root(N), not state_edge(M,N) : state_node(M); not state_type(N,inactive).
+
+        :- state_node(N),
+            not state_root(N),
+            not state_edge(M,N) : state_node(M);
+            not state_type(N,inactive).
+
         state_edge_trans(N1,N2) :- state_edge(N1,N2).
         state_edge_trans(N1,N3) :- state_edge_trans(N1,N2), state_edge(N2,N3).
         :- state_edge_trans(N,N).
         :- state_edge(N1,N3), state_edge(N2,N3), N1 != N2.
         :- state_edge(N,M), M <= N.
-        :- state_node(N), state_node(M), state_type(N,inactive), not state_type(M,inactive).
+
         states_identical(N1,N2) :- state_node(N1), state_node(N2), N1 != N2,
             state(N1,W) : state(N2,W);
             state(N2,W) : state(N1,W).
+
         :- state_edge(N1,N2), state_edge(N1,N3),
             states_identical(N2,N3),
             state_type(N2,T), state_type(N3,T).
 
+        %%% SYMMETRY BREAKING:
+
+        :- state_edge(N1,N2), state_type(N1,inactive).
+        :- state_edge(N1,N2), state_type(N2,inactive).
+        :- state_node(N1), state_node(N2), N2 > N1,
+            state_type(N1,inactive), not state_type(N2,inactive).
+
+        out_degree(W1,D) :- world(W1), D = #count { W2 : relation(W1,W2) }.
+        :- out_degree(W1,D1), out_degree(W2,D2), W2 > W1, D2 < D1.
+        %in_degree(W1,D) :- world(W1), D = #count { W2 : relation(W2,W1) }.
+        %:- out_degree(W1,D), out_degree(W2,D), W2 > W1,
+        %    in_degree(W1,D1), in_degree(W2,D2), D2 < D1.
+
+        siblings(N1,N2) :- state_node(N1), state_node(N2), N1 != N2,
+            state_edge(N,N1), state_edge(N,N2).
+        has_sibling(N1) :- siblings(N1,N2).
+        direct_family(N1,N2) :- siblings(N1,N2).
+        direct_family(N1,N2) :- state_edge(N1,N2).
+        direct_family(N1,N2) :- state_edge(N2,N1).
+        :- state_node(N), state_node(N+1), state_node(N+2),
+            not state_type(N,inactive),
+            not state_type(N+1,inactive),
+            not state_type(N+2,inactive),
+            has_sibling(N+1),
+            not direct_family(N,N+1),
+            not direct_family(N+1,N+2).
+
         #show world/1.
         #show valuation/2.
         #show relation/2.
-        #show state(W) : state(1,W).
+        #show state(W) : state(R,W), state_root(R).
     """
 
     semantics_program = """
@@ -351,7 +396,9 @@ def solve_bsml_sat(
     if use_minimization_heuristics:
         program += heuristics_program
 
-    control = clingo.Control(["--project", "-Wnone", "--heuristic=Domain"])
+    control = clingo.Control(
+        ["--project", "-Wnone", "--heuristic=Domain", "--parallel-mode=4"]
+    )
     control.add("base", [], program)
     if verbose:
         print(".. Grounding ..")
